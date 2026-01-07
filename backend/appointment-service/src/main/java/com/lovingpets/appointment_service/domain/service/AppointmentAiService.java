0888f611796fedb334f0d2e98f3ffa86956402f3
BraiderStid <braidertecnologia86@gmail.com>
2026-01-07 16:34:47 -0500

    feat(appointment-service): scaffold project structure and base architecture

---
+++ backend/appointment-service/src/main/java/com/lovingpets/appointment_service/domain/service/AppointmentAiService.java
@@ -0,0 +1,10 @@
+package com.lovingpets.appointment_service.domain.service;
+
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface AppointmentAiService {
+
+
+
+}
---
+++ backend/appointment-service/src/test/java/com/lovingpets/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovingpets.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
